2c113b9ddd2dae654b69203b65ea1e1e3f75dde4
Deyan Aleksandrov <benglorious@gmail.com>
2017-12-31 13:25:36 +0200

    initial app commit, added app icon and other assets, added color pallete

---
+++ do-it/do-it/Utilities/ColorPallete.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPallete.swift
+//  do-it
+//
+//  Created by Deyan Aleksandrov on 31.12.17.
+//  Copyright © 2017 dido. All rights reserved.
+//
+
+import UIKit
+
+struct ColorPallete {
+    static let ElectricGreen = UIColor(netHex: 0xC6DA02)
+    static let Green = UIColor(netHex: 0x79A700)
+    static let Orange = UIColor(netHex: 0xF68B2C)
+    static let Mustard = UIColor(netHex: 0xE2B400)
+    static let Red = UIColor(netHex: 0xF5522D)
+    static let Pink = UIColor(netHex: 0xFF6E83)
+}
---
+++ do-it/do-it/ViewControllers/MainScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainScreenViewController.swift
+//  do-it
+//
+//  Created by Deyan Aleksandrov on 31.12.17.
+//  Copyright © 2017 dido. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
